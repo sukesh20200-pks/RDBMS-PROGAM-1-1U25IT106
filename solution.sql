a5a8aef08f10b79ece3d46ff4991871a9a15a61f
SUKESH.P <sukesh20200@gmail.com>
2026-08-20 13:25:44 +0530

    progam 1 rdbms

--- solution.sql
+++ solution.sql
@@ -1,1 +1,7 @@
-
+create database un1versetDB
+USE universetDB;
+create table dapartment ( DepartmentID numeric(5) primary key,
+  DepartmentNAME VARCHAR(20),
+  HOD VARCHAR (20)not null 
+);
+describe Department;
